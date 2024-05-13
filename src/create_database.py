2f8c9592806a73181ccc717c4630eb5dd0d508e0
DeclanB1 <declanbarrett23@gmail.com>
2024-05-13 15:07:47 +0800

    Revert "Merge branch 'main' of https://github.com/DeclanB1/Agile-Web-Development"
    This reverts commit 010a11ac51d4edb3c67b016f17521853f5595a4b, reversing
    changes made to 2b29aa98831415939def923aa69eba95766928a5.

--- src/create_database.py
+++ src/create_database.py
@@ -2,39 +2,36 @@ import sqlite3
 import contextlib
 from pathlib import Path
 
-def create_connection(db_file: str):
+
+def create_connection(db_file: str) -> None:
     """ Create a database connection to a SQLite database """
     try:
         conn = sqlite3.connect(db_file)
-        return conn
-    except sqlite3.Error as e:
-        print(f"An error occurred: {e}")
-        return None
+    finally:
+        conn.close()
 
-def create_table(db_file: str):
+
+def create_table(db_file: str) -> None:
     """ Create a table for users """
     query = '''
         CREATE TABLE IF NOT EXISTS users (
             username TEXT PRIMARY KEY,
             password TEXT NOT NULL,
-            email TEXT NOT NULL,
-            fullname TEXT NOT NULL,
-            age INTEGER,
-            preferredlocation,
-            profile_picture TEXT
+            email TEXT
         );
     ''' 
-    conn = create_connection(db_file)
-    if conn is not None:
-        with contextlib.closing(conn) as conn:
-            with conn:
-                conn.execute(query)
 
-def setup_database(name: str):
+    with contextlib.closing(sqlite3.connect(db_file)) as conn:
+        with conn:
+            conn.execute(query)
+
+
+def setup_database(name: str) -> None:
     if Path(name).exists():
-        print('\033[92mDatabase exists. No action required.\033[0m')
         return
 
+    create_connection(name)
     create_table(name)
-    print('\033[91m', 'Creating new example database "users.db"', '\033[0m')
 
+    print('\033[91m', 'Creating new example database "users.db"', '\033[0m')
+    
