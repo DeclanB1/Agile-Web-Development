5ecf0d240c1da94116c72d5af036d70883e22148
Edward Le <leekhoaa@gmail.com>
2024-05-16 13:38:46 +0800

    add flask migrate

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy import UniqueConstraint
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
@@ -19,6 +20,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Set the folder for profile pictures
 UPLOAD_FOLDER = 'profile-pictures'
